9c69aadc6098be50380750237719197269fc4fc5
viktordev965 <viktorgittest@outlook.com>
2025-01-31 10:08:34 -0800

    Create test.cs

---
+++ test.cs
@@ -0,0 +1,4 @@
+public class AnalyticsStandartParamsProvider : IStandardParamsProvider
+{
+    public CloudAnalyticsInfo GetStandardParams(string playerID) => PostLoginStandardParameterProvider.GetCloudAnalyticsInfo();//TODO: Once LuckyVR.VegasInfinite.Analytics becomes its own assembly, eliminate IStandardParamsProvider and call this function directly from GsExecRequest to eliminate needless complexity. 
+}
